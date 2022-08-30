1a95de1f2e91eb1bfab8356e5c376ea2c920a7a2
hallemon@intermade.be <hadrienallemon@gmail.com>
2022-08-30 22:45:50 +0200

    update package

--- src/bot.js
+++ src/bot.js
@@ -272,9 +272,9 @@ Bot.on('voiceStateUpdate', async (oldState, newState) => {
         if (oldState.channelID !==  oldState.guild.me.voice.channelID || newState.channel)
           return;
       
-        if (!oldState.channel.members.size - 1 === 0) {
+        if (oldState.channel.members.size - 1 <= 0) {
             setTimeout(() => { // if 1 (you), wait five minutes
-                if (!oldState.channel.members.size - 1 && connection?.state?.status !== "destroyed") // if there's still 1 member, 
+                if (oldState.channel.members.size - 1 === 0 && connection?.state?.status !== "destroyed") // if there's still 1 member, 
                  destroyConnection(connection);
                }, 1000); 
         }
